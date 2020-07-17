ce7848eb38a3a68e6ef90613f5d0f5d0533d55c2
CAIHAIYAN <36813229+hicai@users.noreply.github.com>
2020-07-17 17:56:34 +0800

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+ created() {
+   this.$axios.get("/data.json").then(res=>{
+      console.log(res)
+    })
+  }
+}
+</script>
---
+++ src/views/lx.vue
@@ -0,0 +1,31 @@
+<template>
+  <div lass="home">
+      <h1>get方法</h1>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'axios2-1',
+  components: {
+
+  },
+ created() {
+//写法一 
+//    this.$axios.get("/data.json").then(res=>{
+//       console.log(res)
+//     })
+
+//写法二
+     this.$axios({
+       methods:'get',
+       url:'/data.json'   
+     }).then(res=>{
+          console.log(res)
+     })
+  }
+}
+</script>
