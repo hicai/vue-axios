ea7d654a1924fae9e3e8c55e0d59b7d33052cc2d
hicai <1021794763@qq,com>
2020-07-23 18:31:17 +0800

    xxx

--- src/views/lx5.vue
+++ src/views/lx5.vue
@@ -17,7 +17,7 @@ export default {
     axios.interceptors.request.use(config=>{
         // 在发送请求前做些什么
         return config
-    },err=>{//请求失败错误时做什么
+    },err=>{//请求失败错误时做什么  请求拦截器错误：发送请求，没有到达后端，比如报404。
         return Promise.reject(err)
     })
     //响应拦截器
@@ -28,16 +28,19 @@ export default {
     })
     axios.get().then(res=>{  //axios.interceptors.response 请求的res数据到then()里面
       console.log(res)  
+    }).catch(err=>{ //响应失败err数据到catch()这里  响应拦截器错误：发送请求后，后端查找不到数据。比如报500
+         console.log(err) 
     })
 
 
-    //取消拦截器
+    //构造拦截器
     let interceptors = axios.interceptors.request.use(config=>{
         config.headers={
             auth:true
         }
          return config
     })
+    //取消拦截器
     axios.interceptors.request.eject(interceptors)
 
     //需要登录状态（token:''）登录的接口
